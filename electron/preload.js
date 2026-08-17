aee903e6aa8eca45fe82a2a97ca7af744eaa01e2
itzzFirenze <joelantony2107@gmail.com>
2026-08-17 17:08:57 +0530

    refactor: remove scratch test files and simplify build scripts in package.json

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Secure IPC methods can be defined here
-  // The frontend can access them via window.electronAPI
-});
